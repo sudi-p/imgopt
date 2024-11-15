cb18ee1409b14589580a9af723672d50652c3e00
Baaz Jhaj <baazjhaj@gmail.com>
2024-11-15 16:49:18 -0700

    brief update

--- ps/aws.py
+++ ps/aws.py
@@ -10,8 +10,8 @@ load_dotenv()
 s3_client = boto3.client(
     's3',
     region_name='us-west-1',
-    aws_access_key_id='AKIAYLRCW5Z53MCRUCWB',
-    aws_secret_access_key='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',
+    aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
     config=boto3.session.Config(signature_version='v4')
 )
 
